22cebd6238941f7bd540588d9216c361822c72bc
Vadim <varem99@mail.ru>
2020-03-28 21:17:13 +0300

    mutex usage

--- 3_Data_race_Mutex/threads.cpp
+++ 3_Data_race_Mutex/threads.cpp
@@ -1,66 +1,28 @@
 #include <thread>
 #include <iostream>
 #include <string>
+#include <mutex>
 
 
-void worker(){
-	std::cout << "It's corona time" << std::endl;
+std::mutex mtx;
+
+void shared_print(std::string msg, int id)
+{
+	mtx.lock();
+	std::cout << msg << id << std::endl;
+	mtx.unlock();
 }
 
-
-// Lambda
-class Functor1
-{
-public:
-	void operator()(){
-		for(int i(0); i > -100; i--)
-			std::cout << "job from t1 " << i << std::endl;
-	}
-};
-
-class Functor2
-{
-public:
-	void operator()(std::string & msg){
-		std::cout << "t1 says: " << msg << std::endl;
-		msg = "t1 washed up its hands";
-	}
-};
+void worker(){
+	for(int i(0); i > -100; i--)
+		shared_print(std::string("job from t1 "), i);
+}
 
 int main()
 {
-	// oversubscription problem
-
-	std::cout << std::thread::hardware_concurrency() << std::endl; // number of real cores
-
-
-	// Functor fct;
-	// std::thread thread_1(fct);
-	
-	// std::thread thread_1(Functor()); This line isn't compilable
-	// std::thread thread_1((Functor1())); it works
-
-	std::string msg = "Watch out and wash hands";
-
-	std::cout << std::this_thread::get_id() << std::endl;
-
-	std::thread thread_1((Functor2()), std::ref(msg));
-	std::cout << thread_1.get_id() << std::endl;
-
-	std::thread thread_2 = std::move(thread_1);
-
-	thread_2.join();
-
-	std::cout << "job from main: " << msg << std::endl;
-
-	// std::thread thread_1(worker); // t1 starts running
-	// using RAII
-	// Wrapper
-	/*try{
-		std::cout << "job from main: " << std::endl;
-	} catch(...){
-		thread_1.join();
-		throw;
-	}*/
+	std::thread t1(worker);
+	for(int i(0); i < 100; ++i)
+		shared_print(std::string("From main: "), i);
+	t1.join();
 	return 0;
 }
